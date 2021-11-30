4ad4f69f74de5c2bd1a04457bdd0beb7efa92df0
tew9ey <tango.tew@ey.com>
2021-11-29 21:10:15 -0600

    push initial files microservice

---
+++ Shopping.Api/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Shopping.Api.Models;
+using Shopping.Api.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _context;
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            // var rnd = new Random();
+            // return Enumerable.Range(1, 5).Select(x => new Product 
+            // {
+            //     Name = "asd"
+            // })
+            // .ToArray();
+            return await _context
+                            .Products
+                            .Find(x =>true)
+                            .ToListAsync();
+        }
+    }
+}
---
+++ Shopping.Api/Repository/ProductContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Shopping.Api.Models;
+
+namespace Shopping.Api.Repository
+{
+    public class ProductContext
+    {
+        private readonly string connectionString;
+        public ProductContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            SendData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+        
+        private  static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "HP lat",
+                    Category = "Computer",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 200.00M,
+                },
+                 new Product()
+                {
+                    Name = "HP lat",
+                    Category = "Computer",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 200.00M,
+                },
+                 new Product()
+                {
+                    Name = "Dell lat",
+                    Category = "Computer",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 600.00M,
+                },
+                 new Product()
+                {
+                    Name = "thinkpad",
+                    Category = "Computer",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 430.00M,
+                },
+                 new Product()
+                {
+                    Name = "Macbook Pro",
+                    Category = "Computer",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 1200.00M,
+                },
+                 new Product()
+                {
+                    Name = "Ipad 5",
+                    Category = "Electronic",
+                    Description = "Description",
+                    ImageFile = "http://images.png",
+                    Price = 100.00M,
+                }
+            };
+        }
+        private static void SendData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+
+        }
+    }
+}
